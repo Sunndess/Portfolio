b7b1a4997a68a1cf2624fa40b7df7a10ab418035
Deysi Campos <102299981+Sunndess@users.noreply.github.com>
2025-01-10 17:51:04 -0500

    V.2 2025
    v2

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fadeElements = document.querySelectorAll('.fade-in');  // Añadir todas las secciones con la clase fade-in
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 });
+
+    fadeElements.forEach(element => observer.observe(element));  // Observar cada sección que tiene la clase fade-in
+});
